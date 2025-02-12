ed317afa81041285db96019912ec883ba2b6ed1f
ada-f <adafang.af@gmail.com>
2025-02-12 11:28:16 -0500

    added versioning to setup env, especially e3nn

--- setup_env.sh
+++ setup_env.sh
@@ -27,15 +27,15 @@ source activate $ENVNAME
 conda install numpy==1.26.4 -y
 pip3 install torch==2.1.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 pip3 install torch_scatter torch_cluster --find-links https://pytorch-geometric.com/whl/torch-2.1.1+cu118.html
-pip3 install tensorboard
-pip3 install e3nn
-pip3 install wandb
-pip3 install scipy
-pip3 install rdkit-pypi
-pip3 install openbabel-wheel
-pip3 install biopython
-pip3 install biotite
+pip3 install tensorboard==2.18.0
+pip3 install e3nn==0.5.1 # possibly not compatible with e3nn > 0.5.4
+pip3 install scipy==1.13.1
+pip3 install rdkit-pypi==2022.9.5
+pip3 install openbabel-wheel==3.1.1.20
+pip3 install biopython==1.84
+pip3 install biotite==0.40.0
 pip3 install atom3d
+pip3 install wandb==0.18.2
 
 # plotting
 pip3 install umap-learn
